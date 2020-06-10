5b6e53de5d11550f2efa22f0144d88e5fd65ffce
Alfan Jauhari <hi.alfanj@gmail.com>
2020-06-10 20:56:31 +0700

    Add Product Detail Page

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,13 @@ function _e(el) {
 _e('.menu-toggler').addEventListener('click', () => {
     _e('.menu-item').classList.toggle('hidden');
 });
+
+function openTab(evt, tabName) {
+    let tabs = document.getElementsByClassName('tab');
+    for(let tab = 0; tab < tabs.length; tab++) {
+        tabs[tab].classList.add('hidden');
+    }
+
+    let name = document.getElementById(tabName);
+    name.classList.toggle('hidden');
+}
